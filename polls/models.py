de8112f3158f27c35d71637bef8973d3ba9f5c27
AlvinOt <thisisalvinhere@gmail.com>
2024-05-31 20:46:57 +0000

    Changes to be committed: 	new file:   polls/migrations/0001_initial.py 	new file:   polls/migrations/0002_rename_choice_choice_choice_text_and_more.py 	modified:   polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Question(models.Model):
-    q_text = models.CharField(max_length=200)
-    p_date = models.DateTimeField("published date")
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("published date")
+
+
+    def __str__(self):
+        return self.question_text
+
+
+    def published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
-    q = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
+    def __str__(self):
+        return self.choice_text
