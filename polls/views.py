a83c190b843d28be0729a963c511cb4708b89728
AlvinOt <thisisalvinhere@gmail.com>
2024-06-04 11:39:26 +0000

    testing index views

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from .models import Question, Choice
 from django.http import HttpResponseRedirect
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -13,7 +13,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+                :5
+                ]
 
 #def detail(request, question_id):
 #    questions = get_list_or_404(Question, pk=question_id)
